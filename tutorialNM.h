cb75cc6e66b4fd96431b20c4bb1e616066936368
kimhaewon1 <khw389@naver.com>
2021-09-26 17:56:04 +0900

    Create tutorialNM.h

---
+++ tutorialNM.h
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------\
+@ Numerical Methods by Young-Keun Kim - Handong Global University
+
+Author           : Haewon Kim
+Created          : 05-03-2021
+Modified         : 02-09-2021
+Language/ver     : C in MSVS2019
+
+Description      : myNM.h
+/----------------------------------------------------------------*/
+
+#ifndef		_MY_NM_H		// use either (#pragma once) or  (#ifndef ...#endif)
+#define		_MY_NM_H
+#define		PI		3.14159265358979323846264338327950288419716939937510582
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+extern double factorial(double _x);
+extern double sinTaylor(double x);
+extern double sindTaylor(double x);
+ectern double sinTaylor(double x);
+
+#endif
